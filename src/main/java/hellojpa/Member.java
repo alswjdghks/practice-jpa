731105612cbb60de4e396f700fcf2af485b2af99
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-06-16 21:13:59 +0900

    첫번째 커밋

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+//@Table(name = "USER")
+public class Member {
+    @Id
+    private long id;
+//    @Column(name = "USER_NAME")
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
