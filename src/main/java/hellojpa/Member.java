82654331a63aa12671cf388c0fd94afe186d16f4
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-06-19 17:45:45 +0900

    1:1매핑관계

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -22,6 +22,10 @@ public class Member {
     @JoinColumn(name = "Team_ID")
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Team getTeam() {
         return team;
     }
